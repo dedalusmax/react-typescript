a7342ce0cf793f23b6122dc42a14b51858d27f85
Ratko Ćosić <RCosic@conet.hr>
2023-03-23 14:28:07 +0100

    type annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface AppProps {
   headerText: string;
   extraText?: string;
 }
 
+interface User {
+  name: string,
+  age: number,
+  country: string,
+  address: {
+    street: string,
+    number: number,
+    zip: string
+  },
+  admin: boolean
+}
+
 export default function App({headerText, extraText = 'default text'} : AppProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchUser = () => setUser({
+    name: 'Ratko',
+    age: 46,
+    country: 'Croatia',
+    address: {
+      street: 'Main st.',
+      number: 22,
+      zip: '22323'
+    },
+    admin: false
+  });
+
   return (
     <>
       <h1>{headerText}</h1>
       <p>{extraText}</p>
+      <br />
+      <button onClick={fetchUser}>Fetch</button>
+      {user && <p>Welcome {user.name}</p>}      
     </>
   );
 }
