745d9d276a4dbb746a47c6371ae059371ceab37e
Ratko Ćosić <RCosic@conet.hr>
2023-03-23 15:54:54 +0100

    add context with dispatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { InputValueProvider } from './InputValueContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <InputValueProvider>
+      <App />
+    </InputValueProvider>
   </React.StrictMode>
 );
