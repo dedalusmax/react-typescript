5163a9db6eed6b4b2c01dd5c5ded79263f6cb7ef
Ratko Ćosić <RCosic@conet.hr>
2023-03-24 09:39:58 +0100

    conditional render

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { InputValueProvider } from './InputValueContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <InputValueProvider>
       <App />
-    </InputValueProvider>
   </React.StrictMode>
 );
